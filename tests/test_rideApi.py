68848e8e58e975c8c6641ef52de34bdbac6d71c4
ruganda <mubaruganda@gmail.com>
2018-06-18 07:37:42 +0300

    Test API can succesfully send a request to join a ride

--- tests/test_rideApi.py
+++ tests/test_rideApi.py
@@ -84,4 +84,24 @@ class TestRide(TestBase):
             self.assertIn(ride['Id'], str(result.data))
 
 
+    def test_join_request_issuccesful(self):
+        """Test API can succesfully send a request to join a ride (POST request)"""
+        response = self.client.post('api/v1/rides/',
+                                      content_type='application/json',
+                                      data=json.dumps(post_ride4))
+
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('api/v1/rides/')
+        self.assertEqual(response.status_code, 200)
+        
+        results = json.loads(response.data.decode())
+    
+
+        for ride in results:
+            
+            response = self.client.post('api/v1/rides/{}/requests'.format(ride['Id']),
+                                        content_type='application/json',
+                                        data=json.dumps({'join':'True'}))
+            self.assertEqual(response.status_code, 201)
+            self.assertIn("A request to join this ride has been sent", str(response.data))
     
