0d2541bb5e18aa13d61b3735913f29284fc37a16
ruganda <mubaruganda@gmail.com>
2018-06-18 06:33:31 +0300

    Correct typo in the comment

--- tests/test_base.py
+++ tests/test_base.py
@@ -6,7 +6,7 @@ from flask import json
 from app import create_app
 
 class TestBase(unittest.TestCase):
-    """ Base class for all test classess """
+    """ Base class for all test classes """
 
     app = create_app('TESTING')
     app.app_context().push()
